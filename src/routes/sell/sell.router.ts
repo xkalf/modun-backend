df357227f24b7cb1e14d0b973b99306182a6a7ae
xkalf <amroos988@gmail.com>
2022-05-03 17:45:56 +0800

    sell router added

--- src/routes/sell/sell.router.ts
+++ src/routes/sell/sell.router.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
+import { createSell, getSell } from './sell.controller'
 
 const sellRouter = Router()
+
+sellRouter.get('/', getSell)
+sellRouter.post('/', createSell)
 
 export default sellRouter
